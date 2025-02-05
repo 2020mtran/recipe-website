0f587388faaca57ec7c4c220cc26d57a14241c37
2020mtran <2020mtran@gmail.com>
2025-02-05 13:37:50 -0800

    Hero section done for all resolutions, started on featured food section

--- src/homepage-components/FeaturedRecipes.jsx
+++ src/homepage-components/FeaturedRecipes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SpringRolls from '../assets/springrolls.webp'
 
-const HeroSection = () => {
+const FeaturedRecipes = () => {
     return (
         <div className="flex flex-row w-full p-3 sm:p-5 md:p-7 lg:p-9 xl:p-11 2xl:p-13 3xl:p-14 items-center">
             <p className='text-chilired text-xl/5 xs:text-2xl/7 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl xl:mr-6 2xl:text-7xl 3xl:text-8xl font-bold'>From <span className='italic'>our</span> kitchen to <span className='italic'>yours</span>, let's make Vietnamese food together!</p>
@@ -10,4 +10,4 @@ const HeroSection = () => {
     );
 };
 
-export default HeroSection;
+export default FeaturedRecipes;
--- src/homepage-components/MainContainer.jsx
+++ src/homepage-components/MainContainer.jsx
@@ -2,15 +2,17 @@ import { React, useState } from 'react';
 import Header from './Header'
 import Sidebar from './Sidebar';
 import HeroSection from './HeroSection';
+import FeaturedRecipes from './FeaturedRecipes';
 
 const MainContainer = ({ children }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full overflow-y-auto">
             <Header isOpen={isOpen} setIsOpen={setIsOpen} />
             <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
             <HeroSection></HeroSection>
+            {/* <FeaturedRecipes></FeaturedRecipes> */}
             {children}
         </div>
     );
