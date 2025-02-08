c003bd294ab3f37811881dd568390be73694de2d
2020mtran <2020mtran@gmail.com>
2025-02-08 12:20:56 -0800

    Fixed bigger resoultions for reviews section

--- src/homepage-components/Reviews.jsx
+++ src/homepage-components/Reviews.jsx
@@ -10,38 +10,38 @@ const Reviews = () => {
             <p className='text-black font-bold text-lg md:text-3xl lg:text-4xl xl:text-5xl 3xl:text-6xl'>Viewer Reviews</p>
             <div className='grid grid-cols-2 grid-rows-2 gap-2'>
                 <div className='flex flex-row'>
-                    <div className='flex flex-col bg-black/80 rounded-lg p-5 w-full gap-1 justify-center'>
-                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl'>You really nailed this! Can't wait to try!</p>
-                        <div className='flex flex-row gap-1 items-center'>
-                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg'>- kerridelorme6605</p>
-                            <FaYoutube className='text-chilired sm:w-5' />
+                    <div className='flex flex-col bg-black/80 rounded-lg p-5 3xl:p-10 w-full gap-1 justify-center'>
+                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl xl:text-3xl 3xl:text-5xl'>You really nailed this! Can't wait to try!</p>
+                        <div className='flex flex-row gap-1 3xl:gap-2 items-center'>
+                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 3xl:text-3xl'>- kerridelorme6605</p>
+                            <FaYoutube className='text-chilired sm:w-5 xl:w-7 xl:h-7' />
                         </div>
                     </div>
                 </div>
                 <div className='flex flex-row'>
-                    <div className='flex flex-col bg-black/80 rounded-lg p-5 w-full gap-1 justify-center'>
-                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl'>Am going to make this come this weekend, I love your recipe best!</p>
-                        <div className='flex flex-row gap-1 items-center'>
-                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg'>- ladylilo6044</p>
-                            <FaYoutube className='text-chilired sm:w-5' />
+                    <div className='flex flex-col bg-black/80 rounded-lg p-5 3xl:p-10 w-full gap-1 justify-center'>
+                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl xl:text-3xl 3xl:text-5xl'>Am going to make this come this weekend, I love your recipe best!</p>
+                        <div className='flex flex-row gap-1 3xl:gap-2 items-center'>
+                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 3xl:text-3xl'>- ladylilo6044</p>
+                            <FaYoutube className='text-chilired sm:w-5 xl:w-7 xl:h-7' />
                         </div>
                     </div>
                 </div>
                 <div className='flex flex-row'>
-                    <div className='flex flex-col bg-black/80 rounded-lg p-5 w-full gap-1 justify-center'>
-                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl'>This looks amazing! I love how you make the recipes so easy to follow.</p>
-                        <div className='flex flex-row gap-1 items-center'>
-                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg'>- nikol210</p>
-                            <FaYoutube className='text-chilired sm:w-5'/>
+                    <div className='flex flex-col bg-black/80 rounded-lg p-5 3xl:p-10 w-full gap-1 justify-center'>
+                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl xl:text-3xl 3xl:text-5xl'>This looks amazing! I love how you make the recipes so easy to follow.</p>
+                        <div className='flex flex-row gap-1 3xl:gap-2 items-center'>
+                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 3xl:text-3xl'>- nikol210</p>
+                            <FaYoutube className='text-chilired sm:w-5 xl:w-7 xl:h-7'/>
                         </div>
                     </div>
                 </div>
                 <div className='flex flex-row'>
-                    <div className='flex flex-col bg-black/80 rounded-lg p-5 w-full gap-1 justify-center'>
-                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl'>Good and easy recipe!</p>
-                        <div className='flex flex-row gap-1 items-center'>
-                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg'>- labonirosalee7800</p>
-                            <FaYoutube className='text-chilired sm:w-5'/>
+                    <div className='flex flex-col bg-black/80 rounded-lg p-5 3xl:p-10 w-full gap-1 justify-center'>
+                        <p className='text-xs xs:text-sm sm:text-base md:text-lg lg:text-2xl xl:text-3xl 3xl:text-5xl'>Good and easy recipe!</p>
+                        <div className='flex flex-row gap-1 3xl:gap-2 items-center'>
+                            <p className='text-xxs xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 3xl:text-3xl'>- labonirosalee7800</p>
+                            <FaYoutube className='text-chilired sm:w-5 xl:w-7 xl:h-7'/>
                         </div>
                     </div>
                 </div>
